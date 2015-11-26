5390ff430a63ab08279ff109f0a9f09d8bacfcbd
Domas Trijonis <domas.trijonis@gmail.com>
2015-11-26 09:52:53 +0100

    Update mjolnir.js

--- mjolnir.js
+++ mjolnir.js
@@ -1,5 +1,4 @@
 'use strict';
-var proxy = require('socket.io-proxy');
 
 var Socket = require('ws')
   , connections = {}
@@ -41,7 +40,7 @@ process.on('message', function message(task) {
   // End of the line, we are gonna start generating new connections.
   if (!task.url) return;
 
-  var socket = proxy.connect(task.url, {
+  var socket = new Socket(task.url, {
     protocolVersion: protocol
   });
 
